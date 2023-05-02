b8c5037ccf856c9cec80690eba0ce9cca0dd7e58
Evan Thomas <evanthomas0292@gmail.com>
2023-05-02 18:27:04 +0100

    Add get and post routes for /albums

--- challenges/music_library_database_app/spec/integration/application_spec.rb
+++ challenges/music_library_database_app/spec/integration/application_spec.rb
@@ -3,11 +3,34 @@ require "rack/test"
 require_relative '../../app'
 
 describe Application do
-  # This is so we can use rack-test helper methods.
   include Rack::Test::Methods
 
-  # We need to declare the `app` value by instantiating the Application
-  # class so our tests work.
   let(:app) { Application.new }
 
+  context "GET /albums" do
+    it "returns all albums" do
+      response = get("/albums")
+
+      expect(response.status).to eq 200
+      expect(response.body).to eq(
+        "Surfer Rosa, Waterloo, Super Trouper, Bossanova, Lover, Folklore" +
+        ", I Put a Spell on You, Baltimore, Here Comes the Sun" +
+        ", Fodder on My Wings, Ring Ring"
+      )
+    end
+  end
+
+  context "POST /albums" do
+    it 'returns 200 OK and adds an artist to the database' do
+      response = post("/albums", title: "Voyage", release_year: "2022", artist_id: "2")
+
+      expect(response.status).to eq(200)
+
+      response = get("/albums")
+      expect(response.body).to include "Voyage"
+    end
+  end
+
+  # context "GET /artists" do
+  #   it "returns "
 end
