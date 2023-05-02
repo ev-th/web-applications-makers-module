e484ed9280b8c7072cdb2963a12d3c3ecd8ecbf0
Evan Thomas <evanthomas0292@gmail.com>
2023-05-02 14:14:38 +0100

    Add hello route get method

--- challenges/hello_web_project/app.rb
+++ challenges/hello_web_project/app.rb
@@ -5,4 +5,9 @@ class Application < Sinatra::Base
   configure :development do
     register Sinatra::Reloader
   end
+
+  get "/hello" do
+    name = params[:name]
+    return "Hello #{name}"
+  end
 end
